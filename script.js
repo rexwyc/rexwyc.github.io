f4b1efb318ef0486820d56f9f0aefbc76af45a9f
rexwyc <110757904+rexwyc@users.noreply.github.com>
2025-09-07 18:07:05 +0800

    hi

---
+++ script.js
@@ -0,0 +1,64 @@
+//Define vars to hold tome values
+let seconds = 0;
+let minutes = 0;
+let hours= 0;
+//Define vars to hold "display"value
+let displaySeconds = 0;
+let displayMinutes = ;
+let displayHours = 0;
+//Define vars to hold setInterval() function
+let interval= null;
+//Define vars to hold stopwatch status
+let status= "stopped";
+//Stopwatch function (logic to determine when to increment next value, etc.)
+function stopWatch(){
+	seconds++;
+	//logic to determine when to increment next value
+	if (seconds/60 ===1){
+		seconds = 0;
+		minutes++;
+		if (minutes/60 === 1){
+			minutes = 0;
+			hours++;
+		}
+	}
+	//IF seconds/minutes/hours are only one dight, add a leading 0 to the value
+		if (second<10){	
+			displaySeconds = "0" + second.toString();
+		}
+		else{
+			displaySeconds = seconds;
+		}
+		if (minutes<10){
+			displayMinutes = "0" + minutes.toString();
+		}
+		else{
+			displayMinutes = minutes;
+		}
+		if (hours<10){
+			displayHours = "0" + hours.toString();
+		}
+		else{
+			displayHours=house;
+		}
+		//Display update time values to user
+		document.getElementById("display").innerHTML = displayHours + ":" + displaySeconds;
+	}
+	function startStop(){
+		if (status === "stopped"){
+			//starts the stopwatch (by calling the setInterval()function)
+			interval = window.setInterval(stopWatch,10);
+			document.getElementById("startStop").innerHTML = "STOP";
+			status = "stopped"
+
+		}
+	}
+	//Function to reset the stopwatch
+	function reset(){
+		window.clearInterval(interval);
+		seconds = 0
+		minutes = 0
+		hours = 0
+		document.getElementById("display").innerHTML = "00:00:00";
+		document.getElementById("startStop").innerHTML = "START";
+	}
